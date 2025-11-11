2cfa0db973e6a81693bd79142116e92f72760830
polillads11 <103006960+polillads11@users.noreply.github.com>
2025-11-11 16:18:26 -0300

    seeder database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,17 +11,17 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    /*public function run(): void
+    public function run(): void
     {
         // User::factory(10)->create();
 
-        User::firstOrCreate(
+        /*User::firstOrCreate(
             ['email' => 'test@example.com'],
             [
                 'name' => 'Test User',
                 'password' => 'password',
                 'email_verified_at' => now(),
             ]
-        );
-    }*/
+        );*/
+    }
 }
